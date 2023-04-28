13f34cae0fc72211f5671a3ad288d5c059a89e87
osgt <129665060+osgt@users.noreply.github.com>
2023-04-28 18:46:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  } 
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
-
 
   // BITCOIN
 
@@ -78,3 +54,44 @@ function openCity(evt, cityName) {
         }
         contributorList.innerHTML = symbols;
       });
+
+
+// TABS
+
+      function openCity(evt, cityName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+// 5$ to BTC
+
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+      } 
+      
+      // Get the element with id="defaultOpen" and click on it
+      document.getElementById("defaultOpen").click();
+    
+
+    const usdAmount = 5; // USD amount for which Bitcoin price is to be displayed
+    // Fetch Bitcoin price from Blockchain.info API
+    fetch('https://blockchain.info/ticker')
+      .then(response => response.json())
+      .then(data => {
+        const bitcoinPrice = parseFloat(data.USD.last); // Extract and parse Bitcoin price from API response
+        const bitcoinAmount = usdAmount / bitcoinPrice; // Calculate Bitcoin amount for given USD amount
+        document.getElementById('bitcoin-price').textContent = '(' + bitcoinAmount.toFixed(8) + ' BTC)'; // Display Bitcoin amount with 8 decimal places
+      })
+      .catch(error => console.error('Error fetching Bitcoin price:', error));
